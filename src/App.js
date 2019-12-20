e8fdcc5ea9da5b2e794c55c0202173b78e162a21
wanku-x <wanku@bk.ru>
2019-12-20 22:41:35 +0300

    Добавлено: Вставил Layout для всех страниц

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ListPage from './Pages/ListPage';
 import ItemPage from './Pages/ItemPage';
 import NotFoundPage from './Pages/NotFoundPage';
+import MainLayout from './Layouts/MainLayout.js';
 
 
 const App = () => (
   <Router>
-    <Switch>
-      <Route path="/" exact component={ListPage} />
-      <Route path="/:id" exact component={ItemPage} />
-      <Route component={NotFoundPage} />
-    </Switch>
+    <MainLayout>
+      <Switch>
+        <Route path="/" exact component={ListPage} />
+        <Route path="/:id" exact component={ItemPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </MainLayout>
   </Router>
 );
 
