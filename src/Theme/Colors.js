1082caa298be9b2141af95cc171178975cad2087
wanku-x <wanku@bk.ru>
2019-12-20 22:43:15 +0300

    Добавлено: Файл с цветами темы

---
+++ src/Theme/Colors.js
@@ -0,0 +1,30 @@
+const textColor = '#111111';
+
+const colors = {
+  primary: {
+    main: '#00aaff',
+    dark: '#0076c6',
+    light: '#6de1ff',
+    contrastText: '#fff',
+  },
+  error: {
+    main: '#ff6163',
+    dark: '#c02235',
+    light: '#ff9a96',
+    contrastText: '#fff',
+  },
+  success: {
+    main: '#97cf26',
+    dark: '#5f9800',
+    light: '#d0ff64',
+    contrastText: '#fff',
+  },
+  info: {
+    main: '#a169f7',
+    dark: '#6837be',
+    light: '#db9eff',
+    contrastText: '#fff',
+  },
+}
+
+export { colors, textColor };
