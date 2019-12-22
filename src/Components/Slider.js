c54bff166efcae876151e1cc4ee3bb4d49c08634
wanku-x <wanku@bk.ru>
2019-12-22 22:33:33 +0300

    Создано: Написал слайдер для картинок

---
+++ src/Components/Slider.js
@@ -0,0 +1,119 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+
+const SliderStyled = styled.div`
+  position: relative;
+  height: ${({ height }) => (
+    Number.isInteger(height) ? height : 300
+  )}px;
+  overflow: hidden;
+  box-shadow: 0 0 6px 1px rgba(64, 64, 64, 0.1);
+`;
+
+const WrapStyled = styled.div`
+  position: relative;
+  display: flex;
+  height: 100%;
+  transition: 0.2s ease-out 0s;
+  left: -${({ shift }) => (
+    Number.isInteger(shift) && shift*100
+  )}%;
+`;
+
+const ItemStyled = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-shrink: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #000;
+`;
+
+const ImageStyled = styled.img`
+  display: block;
+  max-width: 100%;
+  max-height: 100%; 
+  background-image: ${({ src }) => `url('${src}')`};
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center center;
+`;
+
+const ButtonStyled = styled.button`
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  border: none;
+  border-radius: 50%;
+  background-color: rgba(256, 256, 256, 0.4);
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: 20px auto;
+  top: 50%;
+  transform: translateY(-50%);
+  transition: 0.2s ease-out 0s;
+  cursor: pointer;
+  color: #111;
+  ${({ prev }) => prev && `
+    left: 8px;
+    background-image: url('/img/prev.svg');
+  `}
+  ${({ next }) => next && `
+    right: 8px;
+    background-image: url('/img/next.svg');
+  `}
+  ${({ prev, shift }) => prev &&
+    (shift === 0) &&
+    'display: none;'
+  }
+  ${({ next, shift, length }) => next &&
+    (shift === length - 1) &&
+    'display: none;'
+  }
+
+  &:hover {
+    background-color: rgba(256, 256, 256, 0.6);
+  }
+`
+
+const Slider = ({ items=[] }) => {
+  const [shift, setShift] = useState(0);
+
+  const prev = () => (
+    (shift > 0) &&
+    setShift((prevState) => prevState - 1)
+  )
+
+  const next = () => (
+    (shift < (items.length - 1)) &&
+    setShift((prevState) => prevState + 1)
+  )
+
+  return (
+    <SliderStyled>
+      <WrapStyled shift={shift}>
+        {items.map((item, index) => (
+          <ItemStyled key={index}>
+            <ImageStyled src={item} />
+          </ItemStyled>
+        ))}
+      </WrapStyled>
+      <ButtonStyled
+        prev
+        shift={shift}
+        length={items.length}
+        onClick={() => prev()}
+      />
+      <ButtonStyled
+        next
+        shift={shift}
+        length={items.length}
+        onClick={() => next()}
+      />
+    </SliderStyled>
+  )
+}
+
+export default Slider;
