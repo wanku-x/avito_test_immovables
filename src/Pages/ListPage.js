5590a6d0a6c1c024e0ce9bed4d44653d87d4d765
wanku-x <wanku@bk.ru>
2019-12-22 19:08:16 +0300

    Изменено: Описан функционал страницы со списком недвижимости

--- src/Pages/ListPage.js
+++ src/Pages/ListPage.js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col } from 'react-grid-system';
+import { requestGET } from '../Requests';
+import styled from 'styled-components';
+import Card from '../Components/Card';
+import Loader from '../Components/Loader';
 
-const ListPage = () => (
-  <h1>List!</h1>
-);
+
+const MainStyled = styled.main`
+  padding-top: 32px;
+  padding-bottom: 32px;
+`;
+
+const ColStyled = styled(Col)`
+  padding-bottom: 32px;
+`;
+
+const ListPage = () => {
+  const [loading, setLoading] = useState(true);
+  const [immovables, setImmovables] = useState([]);
+  
+  useEffect(() => {
+    requestGET('http://134.209.138.34/items/').then((res) => {
+      setImmovables(res);
+    }).catch((err) => {
+      console.log(err);
+    }).finally(() => {
+      setLoading(false);
+    });
+  });
+
+  return (
+    <MainStyled>
+      <Container>
+        <Row>
+          {immovables.map((item, index) => (
+            <ColStyled sm={6} lg={4} key={index}>
+              <Card
+                id={item.id}
+                address={item.address}
+                title={item.title}
+                previewImage={item.previewImage}
+                price={item.price}
+              />
+            </ColStyled>
+          ))}
+        </Row>
+      </Container>
+      <Loader visible={loading} />
+    </MainStyled>
+  )
+}
 
 export default ListPage;
