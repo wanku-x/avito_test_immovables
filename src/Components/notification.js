37043d97f6bffd5bc17966b57b66e3fee76fdb5e
wanku-x <wanku@bk.ru>
2019-12-23 21:35:31 +0300

    Создано: Компонент для уведомлений

---
+++ src/Components/notification.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'rc-notification/assets/index.css';
+import Notification from 'rc-notification';
+
+const notification = (text) => {
+  Notification.newInstance({}, (n) => {
+    n.notice({
+      content: <span>{ text }</span>,
+      duration: null,
+    });
+  });
+};
+
+export default notification;
